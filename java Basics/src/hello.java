f67eee397e6d96443fe42b811043fa224c81ccb0
muhammedshebinva <muhammedshebinva@gmail.com>
2022-07-20 10:36:59 +0530

    variables

--- java Basics/src/hello.java
+++ java Basics/src/hello.java
@@ -1,5 +1,22 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println(3+3);
+        //this is a comment
+        String name="shebin"; // variable String
+        System.out.println(name);
+
+        int num1=10; //variable integer
+        System.out.println(num1);
+
+        final int num2=20; //not changable variable
+
+        float floatnum=3.14f; //float variable
+        char letter='V'; //char
+        boolean myBool=true; //Boolean
+
+
+
+
     }
 }
