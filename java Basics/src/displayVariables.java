0d44a2af0ab73d5d3346b1e2cf1e288e5237e07e
muhammedshebinva <muhammedshebinva@gmail.com>
2022-07-20 10:50:26 +0530

    java display variables

---
+++ java Basics/src/displayVariables.java
@@ -0,0 +1,24 @@
+public class displayVariables {
+    public static void main(String[] args) {
+        String firstName="shebin";
+        String lastName="VS";
+        System.out.println("Name="+firstName+lastName);
+
+        int x=10; //store whole numbers from -2147483648 to 2147483647.
+        int y=20;
+        System.out.println("sum ="+(x+y));
+
+        //declare many variables
+        int a=10,b=20,c=5,d=2;
+        System.out.println(a+b+c+d);
+
+        byte number=100;
+        System.out.println(number); //store whole numbers from -128 to 127
+
+        short snum=10002; //store whole numbers from -32768 to 32767
+        System.out.println(snum);
+
+        long lNum=1234567890;//store whole numbers from -9223372036854775808 to 9223372036854775807
+        System.out.println(lNum);
+    }
+}
