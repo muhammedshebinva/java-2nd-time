0cabe4f46488121242682d9650ec2dc6c81cf395
muhammedshebinva <muhammedshebinva@gmail.com>
2022-07-20 11:47:47 +0530

    java while,for loop

---
+++ java Basics/src/loopSample.java
@@ -0,0 +1,19 @@
+public class loopSample {
+    public static void main(String[] args) {
+        //do-while loop
+        int i=5;
+
+        do{
+            System.out.println(i);
+            i--;
+        }
+        while(i>0);
+
+        //for loop
+
+        int j=10;
+        for(j=10;j>0;j--){
+            System.out.println(j);
+        }
+    }
+}
