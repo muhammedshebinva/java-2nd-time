63fac48d7d9e44855956f7ff749240894854ae89
muhammedshebinva <muhammedshebinva@gmail.com>
2022-07-23 19:46:20 +0530

    java Method sample

---
+++ java Basics/src/methodSample.java
@@ -0,0 +1,9 @@
+public class methodSample {
+    static  void display(){
+        System.out.println("this is a  method");
+    }
+
+    public static void main(String[] args) {
+        display();
+    }
+}
