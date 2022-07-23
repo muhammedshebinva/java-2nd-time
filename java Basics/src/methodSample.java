f7979ad9081219ae1be42c1fd5edb428c13f385c
muhammedshebinva <muhammedshebinva@gmail.com>
2022-07-23 20:10:43 +0530

    Method calling Parameters passing, return value

--- java Basics/src/methodSample.java
+++ java Basics/src/methodSample.java
@@ -2,8 +2,25 @@ public class methodSample {
     static  void display(){
         System.out.println("this is a  method");
     }
+    //sum function parameter passing
+    static void sum(int num1, int num2){
+        int a=num1;
+        int b=num2;
+        int sum=a+b;
+        System.out.println(a+"+"+b+"="+sum);
+
+    }
+    //return sample
+    static String fullName(String fName,String lName){
+        return fName+lName;
+    }
+
 
     public static void main(String[] args) {
-        display();
+        display(); //method calling
+
+        sum(10,20); //parameters passing
+
+        System.out.println("Full name= "+fullName("shebin","vs"));//Return String value
     }
 }
